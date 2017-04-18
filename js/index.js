21bebf2d57219cf5469711abf22b97700e02c6ff
Franka Rolvink <frolvink@hotmail.comscarlet:git config --global user-name Franka>
2017-04-18 20:47:55 +0200

    My first commit

---
+++ js/index.js
@@ -0,0 +1,19 @@
+$(".button-1").on("click", function (e) {
+    $(".entry-1").slideToggle(300);
+});
+
+$(".button-2").on("click", function (e) {
+    $(".entry-2").slideToggle(300);
+});
+
+$(".button-3").on("click", function (e) {
+    $(".entry-3").slideToggle(300);
+});
+
+$(".button-4").on("click", function (e) {
+    $(".entry-4").slideToggle(300);
+});
+
+$(".button-5").on("click", function (e) {
+    $(".entry-5").slideToggle(300);
+});
